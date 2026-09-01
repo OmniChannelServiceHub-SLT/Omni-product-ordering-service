f52001274530c3def67f4540ead4a5cc729dc123
dasunithimasha <dasunithimasha25@gmail.com>
2026-09-01 21:35:58 +0530

    implement server.js

--- server.js
+++ server.js
@@ -1,1 +1,5 @@
-//test
+require("dotenv").config();
+const app = require("./src/app");
+const connectDB = require("./src/config/db");
+const PORT = process.env.PORT || 3009;
+connectDB().then(() => app.listen(PORT, () => console.log(`Product Ordering Service running on http://localhost:${PORT}`))).catch((error) => { console.error("Failed to start service:", error.message); process.exit(1); });
