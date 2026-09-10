32c71f2896c30791d1e6108ba2fd1c8062c1450e
dasunithimasha <dasunithimasha25@gmail.com>
2026-09-10 22:00:29 +0530

    src/models/TMF622_ProductOrder.js

--- src/models/TMF622_ProductOrder.js
+++ src/models/TMF622_ProductOrder.js
@@ -1,4 +1,64 @@
-const mongoose = require("mongoose");
-const schema = new mongoose.Schema({ externalId: String, description: String, state: { type: String, default: "acknowledged" }, 
-    relatedParty: { type: [mongoose.Schema.Types.Mixed], default: [] }, productOrderItem: { type: [mongoose.Schema.Types.Mixed], default: [] }, channel: { type: [mongoose.Schema.Types.Mixed], default: [] }, legacyOperation: String, legacyPayload: mongoose.Schema.Types.Mixed }, { timestamps: true, strict: false });
-module.exports = mongoose.model("TMF622_ProductOrder", schema);
+const mongoose =
+  require("mongoose");
+
+
+const productOrderSchema =
+  new mongoose.Schema(
+    {
+
+      externalId: {
+        type: String
+      },
+
+      description: {
+        type: String
+      },
+
+      state: {
+        type: String,
+        default: "acknowledged"
+      },
+
+      relatedParty: {
+
+        type: [
+          mongoose.Schema.Types.Mixed
+        ],
+
+        default: []
+      },
+
+      productOrderItem: {
+
+        type: [
+          mongoose.Schema.Types.Mixed
+        ],
+
+        default: []
+      },
+
+      legacyOperation: {
+        type: String
+      },
+
+      legacyPayload: {
+        type:
+          mongoose.Schema.Types.Mixed
+      }
+
+    },
+
+    {
+
+      timestamps: true,
+
+      strict: false
+    }
+  );
+
+
+module.exports =
+  mongoose.model(
+    "TMF622_ProductOrder",
+    productOrderSchema
+  );
